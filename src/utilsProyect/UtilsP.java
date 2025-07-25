744fbf28eb62ade9311a5acc2d293cd63f796621
RolDev-22 <rolandomuagui1629@gmail.com>
2025-07-24 20:52:22 -0600

    Se crea la función que permite generación de botones, solicita por parámetros color y texto del botón

--- src/utilsProyect/UtilsP.java
+++ src/utilsProyect/UtilsP.java
@@ -1,9 +1,13 @@
 package utilsProyect;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Image;
 import java.awt.Toolkit;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 
 /**
  * Autor: Rolando Murillo Aguirre Clase: Utils Descripción: Java class con
@@ -15,13 +19,12 @@ public class UtilsP {
             .getResource("/assets/log_user.png"));
     private final ImageIcon imageIcon = new ImageIcon(getClass()
             .getResource("/assets/log.png"));
-    
-    
+
     private final Image iconUs = imageIconUs.getImage()
             .getScaledInstance(110, 110, Image.SCALE_SMOOTH);
     private final Toolkit tk = Toolkit.getDefaultToolkit();
     private Dimension screnSize = tk.getScreenSize();
-    
+
     public ImageIcon resizeIcon;
     public Image icon;
 
@@ -32,12 +35,25 @@ public class UtilsP {
         return resizeIcon = new ImageIcon(iconUs);
     }
 
-    public Image iconWindow (){
+    public Image iconWindow() {
         return icon = imageIcon.getImage();
     }
-    
-    public int heigthWindow(){
+
+    public int heigthWindow() {
         return screnSize.height;
     }
-    
+
+    public JButton btnMenuGenerate(String textBtnP, Color clrBtnP) {
+
+        JButton btnGenerate = new JButton(textBtnP);
+
+        btnGenerate.setPreferredSize(new Dimension(200, 50));
+        btnGenerate.setFocusable(false);
+        btnGenerate.setFont(new Font("Arial", Font.BOLD, 18));
+        btnGenerate.setForeground(Color.WHITE);
+        btnGenerate.setBackground(clrBtnP);
+
+        return btnGenerate;
+    }
+
 }
